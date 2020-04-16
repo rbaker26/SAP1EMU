4d3097dbb468f7b9d4071389549c07b88815d371
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2020-04-16 12:25:05 -0700

    updated project defs

---
+++ SAP1EMU.Assembler-CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SAP1EMU.Assembler_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SAP1EMU.Engine-CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SAP1EMU.Engine_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
