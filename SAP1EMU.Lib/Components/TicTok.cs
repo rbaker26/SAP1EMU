f89508d7f133989c3d779df6886b004110885ea1
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2020-04-16 12:33:58 -0700

    setup tictok, tics will push to wbus and toks will pull. prevents racecases

--- SAP1EMU.Lib/Components/TicTok.cs
+++ SAP1EMU.Lib/Components/TicTok.cs
@@ -6,5 +6,28 @@ namespace SAP1EMU.Lib.Components
 {
     public struct TicTok
     {
+        public enum State
+        {
+            Tic,
+            Tok
+        };
+
+
+        // TODO - This will prevent racecases when one reg is pushing to wbus and another is pulling
+        // All pushing to Wbus will happen on Tic
+        // All pulling from Wbus will happen on Toc
+        public State ClockState { get; private set; }
+        public void ToggleClockState() 
+        {
+            if(ClockState == State.Tic)
+            {
+                ClockState = State.Tok;
+            }
+            else
+            {
+                ClockState = State.Tic;
+
+            }
+        }
     }
 }
