77c5f1733696efca03a78f3cfdbea4561a0f856a
Bob Baker <31376573+rbaker26@users.noreply.github.com>
2020-06-19 20:19:42 -0700

    fixed duplicate external links bug

--- SAP1EMU.WebApp/wwwroot/js/Views/AboutView.js
+++ SAP1EMU.WebApp/wwwroot/js/Views/AboutView.js
@@ -1,12 +1,17 @@
 ﻿
 
 function openGithub() {
+    console.log("Open GitHub");
+
     const { ipcRenderer } = require("electron");
     ipcRenderer.send("open-github-profile");
 }
 
 
 function openBenEater() {
+    console.log("Open Ben");
+
     const { ipcRenderer } = require("electron");
     ipcRenderer.send("open-ben-eater");
+    
 }
