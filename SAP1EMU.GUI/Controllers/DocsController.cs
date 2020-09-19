9758eca8ce609161b61d010ba6329f05d8a7edc3
rbaker26 <rbaker2614@gmail.com>
2020-09-18 23:22:19 -0700

    integration test cleanup

--- SAP1EMU.GUI/Controllers/DocsController.cs
+++ SAP1EMU.GUI/Controllers/DocsController.cs
@@ -13,43 +13,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace SAP1EMU.GUI.Controllers
 {
-    public class HomeController : Controller
+    public class DocsController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public DocsController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Docs()
         {
             return View();
         }
-        public IActionResult About()
-        {
-            return View();
-        }
-        public IActionResult Emulator()
-        {
-            return View();
-        }
-        public IActionResult Assembler()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult Contributors()
-        {
-            return View();
-        }
-
-
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
