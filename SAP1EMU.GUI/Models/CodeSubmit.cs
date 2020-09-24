39dc0576ece0b06967ba7659c267cf74c520f313
rbaker26 <rbaker2614@gmail.com>
2020-09-23 23:33:13 -0700

    basic db works

---
+++ SAP1EMU.GUI/Models/CodeSubmit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAP1EMU.GUI.Models
+{
+    public class CodeSubmit
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public DateTime submitted_at { get; set; }
+    }
+}
