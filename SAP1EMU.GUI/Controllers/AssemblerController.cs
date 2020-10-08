3c811f80a2769472761e7afe97252875515f76e7
rbaker26 <rbaker2614@gmail.com>
2020-10-07 20:58:06 -0700

    OpenAPI via Swashbuckle basic docs

--- SAP1EMU.GUI/Controllers/AssemblerController.cs
+++ SAP1EMU.GUI/Controllers/AssemblerController.cs
@@ -6,6 +6,7 @@ using SAP1EMU.Lib;
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SAP1EMU.GUI.Controllers
 {
@@ -31,18 +32,30 @@ namespace SAP1EMU.GUI.Controllers
             }
             catch (Exception e)
             {
-               // return NotFound(e.Message + " " + e.InnerException.Message);
                 return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
             }
         }
 
         public class AssemblePacket
         {
+            [Required]
             public List<string> CodeList { get; set; }
+
+            [Required]
             public string SetName { get; set; }
         }
 
-        // POST: api/Assembler
+        /// <summary>
+        /// Compiles SAP1Emu compatable code into Binary
+        /// </summary>
+        /// <param name="assemblePacket">A packet of code and the set name</param>
+        /// <returns>A list of binary strings</returns>
+        /// <response code="200">Returns the list of binary code</response>
+        /// <response code="400">Code contained syntax errors or sets does not exist</response> 
+        /// <response code="500">Server Error. Contact Network Admin</response>  
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [HttpPost]
         public ActionResult Post([FromBody] AssemblePacket assemblePacket)
         {
@@ -50,9 +63,13 @@ namespace SAP1EMU.GUI.Controllers
             {
                 return Ok(Assemble.Parse(assemblePacket.CodeList, assemblePacket.SetName));
             }
-            catch (Exception e)
+            catch (ParseException pe)
             {
-                return BadRequest(e.Message + " " + e.InnerException.Message);
+                return BadRequest(pe.Message + " " + pe.InnerException.Message);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
             }
         }
     }
