84b28d1c67f71b46d0d7d4c4ad8c246d89cd4a13
rbaker26 <rbaker2614@gmail.com>
2020-10-08 20:07:00 -0700

    moved gutter code to site.js

--- SAP1EMU.GUI/wwwroot/js/site.js
+++ SAP1EMU.GUI/wwwroot/js/site.js
@@ -26,3 +26,29 @@ function readFromFile(type, codeMirror, errorBoxID) {
     }
     input.click();
 }
+
+
+function updateGutter(cm) {
+    lineNumber = 1;
+
+    const doc = cm.getDoc();
+    const cursor = doc.getCursor();
+
+    //Since the html is exactly the same i need a way to distinguish between the code mirrors. 
+    //In order for this to work the column needs to be defined as editor if you want it to strip and comment rows only of numbering
+    //and then some issues with copy paste rose with a gutter being in no mans land so it started from 2 on. This contains an array
+    //of the gutter elements found by jquery recursive search with the certain parent of CodeMirror-code since gutter is a child of this
+    gutterElements = $('.editor').find('.CodeMirror-code').find('.CodeMirror-gutter-elt');
+
+    for (i = 0; i < doc.lineCount(); i++) {
+        line = doc.getLine(i);
+
+        //If the line matches a comment style or its a empty line (they hit enter or on a new line) and its not the current line we editing, hide the number in gutter
+        if (line.match(/(^#w*)|(^\s+#w*)/g) || (line.length == 0 && cursor.line != i)) {
+            gutterElements.eq(i).text('');
+        } else {
+            gutterElements.eq(i).text(lineNumber);
+            lineNumber++;
+        }
+    }
+}
