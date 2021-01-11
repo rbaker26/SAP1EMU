d5ed9dd767b6b286da25e066f0e5150ff9d63179
Bob Baker <007dsi@gmail.com>
2021-01-10 21:12:44 -0800

    test env 5

--- SAP1EMU.GUI.TEST/AutomatedUITests.cs
+++ SAP1EMU.GUI.TEST/AutomatedUITests.cs
@@ -12,31 +12,35 @@ namespace SAP1EMU.GUI.Test
     // All of these tests will start from the Index Page
     public class AutomatedUITests : IDisposable
     {
-        private readonly IWebDriver _driver;
-        private string BaseUrl;
+        private static IWebDriver _driver = null;
+        private static string BaseUrl;
         public AutomatedUITests()
         {
-            ChromeOptions chromeOptions = new ChromeOptions();
-            //_driver = new ChromeDriver(chromeOptions);
-            Console.WriteLine("123132132132456465465***********************************************");
-
-            if (Environment.GetEnvironmentVariable("IsDeploymentSlotTest") == "true")
+            if(_driver != null)
             {
-                Console.WriteLine("123132132132456465465$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                //_driver = new ChromeDriver(chromeOptions);
+                Console.WriteLine("123132132132456465465***********************************************");
 
-                BaseUrl = "http://test.sap1emu.net/";
-                chromeOptions.AddArgument("--disable-dev-shm-usage");   // overcome limited resource problems
-                chromeOptions.AddArgument("--no-sandbox");              // Bypass OS security model
-                chromeOptions.AddArgument("--remote-debugging-port=9222");
-            }
-            else
-            {
-                Console.WriteLine("123132132132456465465&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+                if (Environment.GetEnvironmentVariable("IsDeploymentSlotTest") == "true")
+                {
+                    Console.WriteLine("123132132132456465465$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 
-                //BaseUrl = "https://localhost:5001/";
-                BaseUrl = "http://test.sap1emu.net/";
+                    BaseUrl = "http://test.sap1emu.net/";
+                    chromeOptions.AddArgument("--headless");                // No GUI 
+                    chromeOptions.AddArgument("--disable-dev-shm-usage");   // overcome limited resource problems
+                    chromeOptions.AddArgument("--no-sandbox");              // Bypass OS security model
+                                                                            //chromeOptions.AddArgument("--remote-debugging-port=9222");
+                }
+                else
+                {
+                    Console.WriteLine("123132132132456465465&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+                    //BaseUrl = "https://localhost:5001/";
+                    BaseUrl = "http://test.sap1emu.net/";
+                }
+                _driver = new ChromeDriver(chromeOptions);
             }
-            _driver = new ChromeDriver(chromeOptions);
         }
 
 
@@ -49,28 +53,28 @@ namespace SAP1EMU.GUI.Test
             Assert.Equal("Welcome to the SAP1Emu Project", pageText);
         }
 
-        [Fact]
-        public void Navigate_To_About_Page()
-        {
-            _driver.Navigate()
-                .GoToUrl(BaseUrl);
-            _driver.FindElement(By.LinkText("About")).Click();
-            var pageText = _driver.FindElement(By.TagName("h2")).Text;
-            Assert.Contains("About this Project", pageText);
-        }
+        //[Fact]
+        //public void Navigate_To_About_Page()
+        //{
+        //    _driver.Navigate()
+        //        .GoToUrl(BaseUrl);
+        //    _driver.FindElement(By.LinkText("About")).Click();
+        //    var pageText = _driver.FindElement(By.TagName("h2")).Text;
+        //    Assert.Contains("About this Project", pageText);
+        //}
 
-        [Fact]
-        public void Navigate_To_Author_GitHub()
-        {
-            _driver.Navigate()
-                .GoToUrl(BaseUrl);
-            _driver.FindElement(By.LinkText("About")).Click();
+        //[Fact]
+        //public void Navigate_To_Author_GitHub()
+        //{
+        //    _driver.Navigate()
+        //        .GoToUrl(BaseUrl);
+        //    _driver.FindElement(By.LinkText("About")).Click();
 
-            // Nav to GitHub Profile
-            _driver.FindElement(By.CssSelector(".card")).FindElement(By.LinkText("Follow")).Click();
-            _driver.SwitchTo().Window(_driver.WindowHandles[1]);
-            Assert.Contains("rbaker26", _driver.Title);
-        }
+        //    // Nav to GitHub Profile
+        //    _driver.FindElement(By.CssSelector(".card")).FindElement(By.LinkText("Follow")).Click();
+        //    _driver.SwitchTo().Window(_driver.WindowHandles[1]);
+        //    Assert.Contains("rbaker26", _driver.Title);
+        //}
 
         public void Dispose()
         {
